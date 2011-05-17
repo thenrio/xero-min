b3376f9a1e588f7c27e866e62b6cfc0683e9cd59
thierry <thierry.henrio@gmail.com>
2011-05-17 21:59:14 +0200

    url_for can take a hash : invoice: 'INV-001' so that we have get url for this invoice

--- lib/xero-min/urls.rb
+++ lib/xero-min/urls.rb
@@ -1,7 +1,16 @@
 module XeroMin
   module Urls
-    def url_for(sym)
-      "https://api.xero.com/api.xro/2.0/#{sym.to_s.capitalize}"
+    # Public : use plural to get a collection or use singular and value
+    # url_for(:invoices) OR url_for(invoice: 'INV-001')
+    def url_for(sym_or_hash)
+      key, value = case sym_or_hash
+      when Hash
+        sym_or_hash.first
+      else
+        sym_or_hash
+      end
+      base = "https://api.xero.com/api.xro/2.0/#{key.to_s.capitalize}"
+      value ? "#{base}s/#{value}" : base
     end
   end
 end
--- spec/xero-min/urls_spec.rb
+++ spec/xero-min/urls_spec.rb
@@ -6,4 +6,7 @@ describe XeroMin::Urls do
   {contact: 'Contact', contacts: 'Contacts'}.each do |k,v|
     specify {url_for(k).should == "https://api.xero.com/api.xro/2.0/#{v}"}
   end
+  it "can take a hash and and id" do
+    url_for(invoice: 'INV-001').should == "https://api.xero.com/api.xro/2.0/Invoices/INV-001"
+  end
 end
